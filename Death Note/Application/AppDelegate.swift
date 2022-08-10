39bcf5d69ad2295ed0dcb4213a95567ea91d2a63
Artem <Serebryakov>
2022-08-10 19:08:45 +0300

    made tableview, set constrains, and extention delegate

--- Death Note/Application/AppDelegate.swift
+++ Death Note/Application/AppDelegate.swift
@@ -15,14 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let rootVC = ViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = navVC
         window?.makeKeyAndVisible()
+        
         window?.overrideUserInterfaceStyle = .light
         
         return true
     }
 
 
-
+    
 }
 
--- Death Note/Controllers/ViewController.swift
+++ Death Note/Controllers/ViewController.swift
@@ -9,12 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let idCell = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        title = "Death Note"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        setConstrains()
+        
+        //Registers a class to use in creating new table cells.
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: idCell)
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // кол-во строк в секции
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idCell, for: indexPath) as! TableViewCell
+
+        cell.textLabel?.text = "Cell"
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        80
+//    }
+}
+
+extension ViewController {
+    func setConstrains() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        
+        ])
+    }
+        
+}
